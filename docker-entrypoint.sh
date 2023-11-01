6b62a38e42594b8836d9b1f1c15e59441a80cb4b
João Pedro Prado <joaopedro0128@hotmail.com>
2023-11-01 02:46:15 -0300

    feat: base starkbank chart
    Signed-off-by: João Pedro Prado <joaopedro0128@hotmail.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,22 +4,39 @@ if [ -z "$STARKBANK_PRIVATE_KEY" ]; then
   export STARKBANK_PRIVATE_KEY=$(cat $STARKBANK_PRIVATE_KEY_FILE)
 fi
 
+if ["$ENV" = "dev"]; then
+  if [ "$1" = "worker" ]; then
+    poetry run \
+      python -m debugpy --listen 0.0.0.0:5679 -m \
+        celery -A starkbank_backend worker -l INFO
+  elif [ "$1" = "beat" ]; then
+    poetry run \
+      python -m debugpy --listen 0.0.0.0:5680 -m \
+        celery -A starkbank_backend beat -l INFO
+  else
+    poetry run python -m manage migrate
+
+    mkdir log
+    touch /app/log/access.log
+
+    poetry run \
+      python -m debugpy --listen 0.0.0.0:5678 -m \
+        gunicorn starkbank_backend.asgi:application -k uvicorn.workers.UvicornWorker -w 3 \
+        -b 0.0.0.0:8000 --log-level=info --access-logfile=/app/log/access.log -t 120
+  fi
+fi
+
 if [ "$1" = "worker" ]; then
-  poetry run \
-    python -m debugpy --listen 0.0.0.0:5679 -m \
-      celery -A starkbank_backend worker -l INFO
+  poetry run python -m celery -A starkbank_backend worker -l INFO
 elif [ "$1" = "beat" ]; then
-  poetry run \
-    python -m debugpy --listen 0.0.0.0:5680 -m \
-      celery -A starkbank_backend beat -l INFO
+  poetry run python -m celery -A starkbank_backend beat -l INFO
 else
   poetry run python -m manage migrate
 
   mkdir log
   touch /app/log/access.log
 
-  poetry run \
-    python -m debugpy --listen 0.0.0.0:5678 -m \
-      gunicorn starkbank_backend.asgi:application -k uvicorn.workers.UvicornWorker -w 3 \
-      -b 0.0.0.0:8000 --log-level=info --access-logfile=/app/log/access.log -t 120
+  poetry run python -m \
+    gunicorn starkbank_backend.asgi:application -k uvicorn.workers.UvicornWorker -w 3 \
+    -b 0.0.0.0:8000 --log-level=info --access-logfile=/app/log/access.log -t 120
 fi
