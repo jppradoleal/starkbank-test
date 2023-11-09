9db149ed7c4721570e6d35cb54802a61f0fbc300
João Pedro Prado <joaopedro0128@hotmail.com>
2023-11-09 17:03:48 -0300

    chore: typing

--- payments/services/starkbank.py
+++ payments/services/starkbank.py
@@ -9,20 +9,20 @@ from payments import exceptions, tasks
 class StarkbankService:
     __project = None
 
-    def __init__(self, private_key) -> None:
+    def __init__(self, private_key: str) -> None:
         self.__project = starkbank.Project(
             environment="sandbox", id="6214224581754880", private_key=private_key
         )
 
         starkbank.user = self.__project
 
-    def create_invoices(self, invoices: list[Invoice]):
+    def create_invoices(self, invoices: list[Invoice]) -> list[Invoice]:
         return starkbank.invoice.create(invoices)
 
-    def create_transfers(self, transfers: list[Transfer]):
+    def create_transfers(self, transfers: list[Transfer]) -> list[Transfer]:
         return starkbank.transfer.create(transfers)
 
-    def parse_event(self, payload, signature) -> starkbank.Event:
+    def parse_event(self, payload: str, signature: str) -> starkbank.Event:
         try:
             event = starkbank.event.parse(payload, signature)
         except error.InvalidSignatureError:
@@ -30,7 +30,7 @@ class StarkbankService:
 
         return event
 
-    def handle_invoice_event(self, data, digital_signature):
+    def handle_invoice_event(self, data: str, digital_signature: str) -> None:
         event: Event = self.parse_event(data, digital_signature)
 
         if event.subscription != "invoice":
@@ -44,7 +44,7 @@ class StarkbankService:
         tasks.send_transfers.delay(amount=amount - fees)
 
 
-def get_private_key_content():
+def get_private_key_content() -> str:
     return os.getenv("STARKBANK_PRIVATE_KEY")
 
 
